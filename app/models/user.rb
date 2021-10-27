ce6f662fcf7bc534bbeb1c4021ac017d5127e098
Enselme <eaguidigodo@gmail.com>
2021-10-27 10:02:12 +0100

    added the top page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   #  :lockable, :timeoutable and :omniauthable
   has_many :budgets, dependent: :delete_all
-  has_many :needs, dependent: :delete_all
   devise :database_authenticatable, :registerable, :trackable,
          :recoverable, :rememberable, :validatable
 end
