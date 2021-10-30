d1c38d5bbed964c8854357dc66c245ec36e6c7c9
Enselme <eaguidigodo@gmail.com>
2021-10-30 21:27:43 +0100

    React is working well

--- app/javascript/components/Page.js
+++ app/javascript/components/Page.js
@@ -3,16 +3,13 @@ import PropTypes from "prop-types"
 class Page extends React.Component {
   render () {
     return (
-      <React.Fragment className="text-info h2" >
-        Title: {this.props.title}
-        Description: {this.props.description}
-      </React.Fragment>
+      <div class="alert alert-success" role="alert">
+         <h4 class="alert-heading">{this.props.title}</h4>
+        <p>{this.props.description}.</p>
+</div>
     );
   }
 }
 
-Page.propTypes = {
-  title: PropTypes.string,
-  description: PropTypes.node
-};
+
 export default Page
